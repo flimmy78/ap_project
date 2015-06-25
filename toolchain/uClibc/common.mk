3061e71c2a698be4f03aaf22e6e119549afc522d
zhangsiyu <zhangsiyu@debian6.(none)>
2015-06-25 17:06:34 -0400

    modfiy for libbacktace 2015-6-25

--- toolchain/uClibc/common.mk
+++ toolchain/uClibc/common.mk
@@ -76,10 +76,12 @@ define Host/Configure
 	$(GEN_CONFIG) > $(HOST_BUILD_DIR)/.config.new
 	$(SED) 's,^KERNEL_HEADERS=.*,KERNEL_HEADERS=\"$(BUILD_DIR_TOOLCHAIN)/linux-dev/include\",g' \
 		-e 's,^.*UCLIBC_HAS_FPU.*,UCLIBC_HAS_FPU=$(if $(CONFIG_SOFT_FLOAT),n,y),g' \
+		-e 's,^.*DO_XSI_MATH.*,DO_XSI_MATH=y,g' \
 		-e 's,^.*UCLIBC_HAS_SOFT_FLOAT.*,UCLIBC_HAS_SOFT_FLOAT=$(if $(CONFIG_SOFT_FLOAT),y,n),g' \
 		-e 's,^.*UCLIBC_HAS_SHADOW.*,UCLIBC_HAS_SHADOW=$(if $(CONFIG_SHADOW_PASSWORDS),y,n),g' \
 		-e 's,^.*UCLIBC_HAS_LOCALE.*,UCLIBC_HAS_LOCALE=$(if $(CONFIG_BUILD_NLS),y,n),g' \
 		$(HOST_BUILD_DIR)/.config.new
+	$(SED) 's,^# UCLIBC_HAS_BACKTRACE is not set,UCLIBC_HAS_BACKTRACE=y,' $(TOPDIR)/toolchain/uClibc/config-0.9.33.2/common
 	cmp -s $(HOST_BUILD_DIR)/.config.new $(HOST_BUILD_DIR)/.config.last || { \
 		cp $(HOST_BUILD_DIR)/.config.new $(HOST_BUILD_DIR)/.config && \
 		$(MAKE) -C $(HOST_BUILD_DIR) oldconfig KBUILD_HAVE_NLS= HOSTCFLAGS="-DKBUILD_NO_NLS" && \
