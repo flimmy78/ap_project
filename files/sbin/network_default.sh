d0516db1eb88bb6e5c5d56a08b2fe215ce64f6e6
zhangsiyu <zhangsiyu@debian6.(none)>
2015-07-16 14:24:01 -0400

    modify for network local and default

--- files/sbin/network_default.sh
+++ files/sbin/network_default.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 main() {
 	cp /rom/etc/config/network /etc/config/network
+	cp /rom/etc/config/firewall /etc/config/firewall
 	/etc/init.d/network reload
+	/etc/init.d/firewall restart > /dev/null 2>&1
 	/etc/jsock/jcmd.sh syn "route del default gw 1.0.0.1" 2>/dev/null
 	/etc/jsock/jcmd.sh syn "start_3g" &
 }
--- files/sbin/network_local.sh
+++ files/sbin/network_local.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 main() {
 	cp /etc/config/network.dev /etc/config/network
-	/etc/init.d/network reload
+	cp /etc/config/firewall.dev /etc/config/firewall
+
+	/etc/init.d/network reload > /dev/null 2>&1
+	/etc/init.d/firewall restart > /dev/null 2>&1
 	/etc/jsock/jcmd.sh syn "stop_3g"
 	/etc/jsock/jcmd.sh syn "route del default ppp0" 2>/dev/null
 	/etc/jsock/jcmd.sh syn "route add default gw 1.0.0.1" 2>/dev/null
