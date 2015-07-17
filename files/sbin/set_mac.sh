f22ae88de03e3f498bebb697206f9dc970c326a7
zhangsiyu <zhangsiyu@debian6.(none)>
2015-07-17 11:26:08 -0400

    modify for set mac 2015-7-17

--- files/sbin/set_mac.sh
+++ files/sbin/set_mac.sh
@@ -2,12 +2,14 @@
 
 set_ap_mac()
 {
-	local mdmac=$(/etc/jsock/jcmd.sh syn atenv infos/product/mac)
+	local err=0
+	local mdmac=$(/usr/sbin/get_sysinfo mac)
 	local apmac=$(/usr/bin/partool -part mtd7 -show product.mac)
 	
 	if [[ "${apmac}" != "${mdmac}" ]] ;then
 		apmac="${mdmac}"
-		/usr/bin/partool -part mtd7 -new product.mac "${apmac}"
+		/usr/bin/partool -part mtd7 -new product.mac "${apmac}";err=$?
+		[[ ${err} -ne 0 ]] && /usr/bin/partool -part mtd7 -empty
 	fi
 }
 
