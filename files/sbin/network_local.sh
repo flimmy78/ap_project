a78615138d4a95bf6ec23cd5b5eb6b8781935924
liuhj <liuhuijun@e830.(none)>
2015-07-24 11:09:32 +0800

    modify for resolv and route

--- files/sbin/network_local.sh
+++ files/sbin/network_local.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 main() {
-	cp /etc/config/network.dev /etc/config/network
-	cp /etc/config/firewall.dev /etc/config/firewall
+	/etc/jsock/jcmd.sh asyn "stop_3g"
+	/etc/jsock/jcmd.sh asyn "route del default ppp0" 2>/dev/null
+	/etc/jsock/jcmd.sh asyn "route add default gw 1.0.0.1" 2>/dev/null
+
+	cp /rom/etc/config/network.dev /etc/config/network
+	cp /rom/etc/config/firewall.dev /etc/config/firewall
 
 	/etc/init.d/network reload > /dev/null 2>&1
 	/etc/init.d/firewall restart > /dev/null 2>&1
-	/etc/jsock/jcmd.sh syn "stop_3g"
-	/etc/jsock/jcmd.sh syn "route del default ppp0" 2>/dev/null
-	/etc/jsock/jcmd.sh syn "route add default gw 1.0.0.1" 2>/dev/null
 }
 
 main "$@"
