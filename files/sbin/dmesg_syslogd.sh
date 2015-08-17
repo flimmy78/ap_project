3310385153e8a535a20f175fccfde5c3557d4613
liuhuijun <liuhuijun@e830.(none)>
2015-08-17 11:18:32 +0800

    modify dmesg_syslogd.sh, and add syslogd in .config

--- files/sbin/dmesg_syslogd.sh
+++ files/sbin/dmesg_syslogd.sh
@@ -5,12 +5,17 @@
 main() {
 	/etc/jsock/jcmd.sh syn /usr/sbin/syn_ap_time.sh
 	local file=${SYNTIME}
+	local i=0
 	syslogd
 
 	# todo: check & create SYNTIME here
 	while [[ ! -f ${file} ]]
 	do
 		sleep 9
+		((i++))
+		if ((i>6)); then
+			date '+%F-%H:%M:%S' > ${file}
+		fi
 	done
 
 	save_init_log
