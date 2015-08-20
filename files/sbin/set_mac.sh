b43309ba425eacb6fdaf78f567198f1322de81cd
liuhuijun <liuhuijun@e830.(none)>
2015-08-20 16:18:28 +0800

    modify for ap mac and ap 9531 2g txpower

--- files/sbin/set_mac.sh
+++ files/sbin/set_mac.sh
@@ -3,13 +3,16 @@
 set_ap_mac()
 {
 	local err=0
-	local mdmac=$(/etc/jsock/jcmd.sh syn /usr/sbin/get_sysinfo mac)
-	local apmac=$(/usr/bin/partool -part mtd7 -show product.mac)
+	local mdmac=$(/etc/jsock/jcmd.sh syn /usr/sbin/get_sysinfo mac 2> /dev/null)
+	local apmac=""
+
+	apmac=$(partool -part mtd7 -show product.mac 2> /dev/null);err=$?
+	[[ ${err} -ne 0 ]] && partool -part mtd7 -empty
 	
-	if [[ "${apmac}" != "${mdmac}" ]] ;then
+	if [[ "${apmac}" != "${mdmac}" && "${mdmac}" ]] ;then
 		apmac="${mdmac}"
-		/usr/bin/partool -part mtd7 -new product.mac "${apmac}";err=$?
-		[[ ${err} -ne 0 ]] && /usr/bin/partool -part mtd7 -empty
+		partool -part mtd7 -new product.mac "${apmac}";err=$?
+		[[ ${err} -ne 0 ]] && partool -part mtd7 -delete product.mac
 	fi
 }
 
