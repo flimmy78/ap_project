039597574810b0b0d056c747fde4bc205af1ec07
liuhuijun <liuhuijun@e830.(none)>
2015-08-31 18:53:43 +0800

    modify set_mac.sh

--- files/sbin/set_mac.sh
+++ files/sbin/set_mac.sh
@@ -4,15 +4,12 @@ set_ap_mac()
 {
 	local err=0
 	local mdmac=$(/etc/jsock/jcmd.sh syn /usr/sbin/get_sysinfo mac 2> /dev/null)
-	local apmac=""
-
-	apmac=$(partool -part mtd7 -show product.mac 2> /dev/null);err=$?
-	[[ ${err} -ne 0 ]] && partool -part mtd7 -empty
+	local apmac=$(partool -part mtd7 -show product.mac 2> /dev/null)
 	
 	if [[ "${apmac}" != "${mdmac}" && "${mdmac}" ]] ;then
 		apmac="${mdmac}"
 		partool -part mtd7 -new product.mac "${apmac}";err=$?
-		[[ ${err} -ne 0 ]] && partool -part mtd7 -delete product.mac
+		[[ ${err} -ne 0 ]] && partool -part mtd7 -empty 
 	fi
 }
 
